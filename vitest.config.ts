e89cb13d0dbb58da8b850e4cd2cc5c766e114485
Matheus Rocha <matheus.rocha89@gmail.com>
2025-01-12 18:25:06 +0000

    chore: adding coverage
    Adding coverage to the project to send to coverage services

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,20 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    include: ["**/*.test.tsx"],
+    include: ["./src/**/*.test.tsx"],
     globals: true,
     environment: "jsdom",
     setupFiles: ["./test.setup.js"],
+    coverage: {
+      exclude: [
+        "commitlint.config.js",
+        "./example/**",
+        "./src/CopyText/index.ts",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 });
